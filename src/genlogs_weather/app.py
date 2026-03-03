22557419ccd65c778e43706e4d2b61757e4dd90e
Mac <wmiller@condata.com>
2026-03-03 08:37:05 -0600

    add logging logic, use dotenv for standard/override

--- src/genlogs_weather/app.py
+++ src/genlogs_weather/app.py
@@ -2,9 +2,17 @@
 from __future__ import annotations
 import logging
 import os
-from dataclasses import dataclass
 from dotenv import load_dotenv
-from typing import List, Tuple
+from genlogs_weather.config.locations import LOCATIONS #config/locations.py 
 
 # Load .env for secure configs
 load_dotenv()
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper() #Default to INFO if not set; allows override via environment variable
+
+# Configure logging with a standard format and level set by LOG_LEVEL environment variable
+logging.basicConfig(
+    level=LOG_LEVEL,
+    format="%(asctime)s [%(levelname)s] %(name)s - %(message)s", # Log format: timestamp, log level, logger name, and message
+)
+logger = logging.getLogger("genlogs_weather")
