ae7fab7f15212885b2963b28cb5a8abcc7055556
Mac <wmiller@condata.com>
2026-03-04 08:52:05 -0600

    dbt init, setup

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-03-04T14:08:22.550012100+00:00
+-- finished_at: 2026-03-04T14:08:22.661514200+00:00
+-- elapsed: 111ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c2cdd0-0000-dc3d-0017-59f70007119a
+-- desc: not provided
+select 1 as id;
