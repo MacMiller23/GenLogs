fbaf45534c803dffdae29f12cfad98b9f9551e52
Mac <wmiller@condata.com>
2026-03-05 09:11:49 -0600

    locations.py config comments

--- src/genlogs_weather/config/locations.py
+++ src/genlogs_weather/config/locations.py
@@ -1,8 +1,8 @@
-from __future__ import annotations
-from dataclasses import dataclass
-from typing import List, Tuple
+from __future__ import annotations # allows for using the class name 'Location' in type hints within the class definition itself
+from dataclasses import dataclass  # auto generates  __init__() and __repr__() for classes.  use here to define a simple class for locations, makes code cleaner
+from typing import List, Tuple     # not strictly needed, but can be helpful for type hinting lists of tuples, which is what we use for locations in this project
 
-@dataclass(frozen=True)
+@dataclass(frozen=True) # Data shouldn't change after creation
 class Location:
     """Represents fixed locations for each location, 4 decimal places gives 10m precision, which should be more than enough.
     In the real world, this would probably come from a metadata table
@@ -11,7 +11,8 @@ class Location:
     lat: float
     lon: float
 
-# Source: known public coordinates for each location; got from google. treated as configuration inputs to Open-Meteo.
+# Known public coordinates for each location; got from google
+# in a PROD scenario, these would likely come from a metadata table instead of being hardcoded in the codebase
 LOCATIONS: List[Location] = [
     Location("Atlanta, GA", 33.7490, -84.3880),
     Location("New York, NY", 40.7128, -74.0060),
